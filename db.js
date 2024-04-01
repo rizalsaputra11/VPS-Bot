a281ce7543d7c1691493976789f8280dd1ca1c59
Bas <55401336+BasToTheMax@users.noreply.github.com>
2024-04-01 12:14:31 +0000

    Add database

---
+++ db.js
@@ -0,0 +1,40 @@
+const mongoose = require('mongoose');
+mongoose.connect(process.env.MONGO_URL);
+
+const User = mongoose.model('User', {
+    userID: String,
+    balance: Number,
+    
+    vpsLimit: Number,
+    
+    plan: String // free
+});
+
+const VPS = mongoose.model('VPS', {
+    userID: String,
+    ip: String,
+    
+    proxID: Number,
+    
+    name: String,
+    
+    ram: Number,
+    cpu: Number,
+    disk: Number,
+    
+    hasUsed: Boolean,
+    usedCode: Number,
+    
+    expiry: Number, // Time to expire after lastRenew
+    password: String,
+    
+    cost: Number,
+    lastCost: Number,
+    state: String, // queued / created
+    node: String
+});
+
+module.exports = {
+    User,
+    VPS
+};
