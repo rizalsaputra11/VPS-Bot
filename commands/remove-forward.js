e3c40dacad85ba95a6a1c6acfdadd055c1e38756
Bas <55401336+BasToTheMax@users.noreply.github.com>
2024-04-02 17:23:34 +0000

    Add remove forward

---
+++ commands/remove-forward.js
@@ -0,0 +1,79 @@
+const { SlashCommand } = require('slashctrl');
+const { time } = require('discord.js');
+const lib = require('../lib');
+
+class CMD extends SlashCommand {
+
+    constructor() {
+        super();
+        
+        // this.guilds = ["1211544398219976724"];
+        
+        this.setName("removeforward");
+        this.setDescription("Remove a port forward");
+
+        this.addIntegerOption(option =>
+            option.setName('id')
+                .setDescription('VPS ID')
+				.setRequired(true));
+
+        this.addIntegerOption(option =>
+            option.setName('port')
+                .setDescription('internal port (e.g. 80)')
+                .setRequired(true));
+
+        this.requiresAdmin = false;
+    }
+    
+    async execute(interaction) {
+        if (await lib.checkAdmin(this, interaction)) return;
+
+        const db = require('../db');
+
+        var user = await lib.getUser(interaction);
+
+        var id = interaction.options.getInteger('id');
+        var port = interaction.options.getInteger('port');
+
+        var VPS = await db.VPS.findOne({
+            shortID: id,
+            userID: interaction.user.id
+        });
+        if (!VPS) return await lib.error(interaction, 'VPS not found');
+
+        var Port = await db.Port.find({
+            vpsID: VPS._id,
+            intPort: port
+        });
+
+        if (!Port) return await lib.error(interaction, 'Port not found');
+
+        sshPort.isUsed = false;
+        sshPort.intPort = null;
+        sshPort.vpsID = null;
+        await sshPort.save();
+
+        await interaction.deferReply();
+
+        var queue = interaction.client.opsQueue[VPS.node];
+
+        if (!queue) return await lib.error(interaction, 'Node not found?', true);
+
+        await interaction.editReply('Adding to queue...');
+        
+        var job = await queue.add(`vps_${interaction.user.id}-${Date.now()}`, {
+            action: 'rforward',
+            proxID: VPS.proxID,
+            ip: VPS.ip,
+            port: sshPort.port,
+            intPort: port,
+            userID: interaction.user.id,
+            portID: sshPort._id
+        });
+
+        interaction.editReply(`**QUEUED**\nYour request to remove the forwarded port has been placed in the queue with iD ${job.id} and will get processed shortly. External port: ${sshPort.port}`);
+    }
+
+}
+
+module.exports = { default: CMD };
