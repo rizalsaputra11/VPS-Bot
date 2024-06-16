6c6c2405af8bc8aaff368a1f689c7e9d84de96b1
Bas <55401336+BasToTheMax@users.noreply.github.com>
2024-06-16 14:20:40 +0200

    Add ban

---
+++ commands/ban.js
@@ -0,0 +1,70 @@
+const { SlashCommand } = require('slashctrl');
+const lib = require('../lib');
+
+
+var randomip = require('random-ip');
+var generator = require('generate-password');
+
+class CMD extends SlashCommand {
+
+    constructor() {
+        super();
+        
+        // this.guilds = ["1211544398219976724"];
+        
+        this.setName("ban");
+        this.setDescription("[ADMIN] Delete vps & ban user");
+
+        this.addIntegerOption(option =>
+            option.setName('id')
+                .setDescription('VPS ID')
+				.setRequired(true));
+
+      this.addStringOption(option =>
+            option.setName('reason')
+                .setDescription('Ban reason')
+				.setRequired(true));
+      
+        this.requiresAdmin = true;
+    }
+    
+    async execute(interaction) {
+        if (await lib.checkAdmin(this, interaction)) return;
+
+        var user = await lib.getUser(interaction);
+
+        var ID = interaction.options.getInteger('id');
+
+        const db = require('../db');
+
+        var VPS = await db.VPS.findOne({
+            shortID: ID
+        });
+
+        if (!VPS) return await lib.error(interaction, 'VPS not found');
+
+        console.log('vps', VPS);
+
+        if (VPS.state != 'created') return await lib.error(interaction, 'VPS is not created ' + VPS.state);
+        
+        await interaction.deferReply();
+
+        vps.expiry = 0;
+        await vps.save();
+
+        var user = await db.User.findOne({
+            userID: vps.userID
+        });
+
+        var res = interaction.options.getString('reason');
+        user.vpsLimit = 0;
+        user.isBanned = true;
+        user.banReason = res;
+        await user.save();
+      
+        interaction.editReply(`Updated expiry & banned user`);
+    }
+
+}
+
+module.exports = { default: CMD };
